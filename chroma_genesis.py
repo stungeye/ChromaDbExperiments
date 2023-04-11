75ca83679cdf4f2929a2fda7db94e1ba44dfc192
Kyle Geske <stungeye@gmail.com>
2023-04-11 11:28:00 -0500

    After adding auto-formatting with black.

--- chroma_genesis.py
+++ chroma_genesis.py
@@ -1,27 +1,29 @@
 import chromadb
-import glob
 import json
-import pprint
+
 
 def read_json_from_file(filename):
-    with open(filename, 'r', encoding='utf8') as f:
+    with open(filename, "r", encoding="utf8") as f:
         data = f.read()
         return json.loads(data)
+
 
 genesis_documents = []
 genesis_metadata = []
 genesis_ids = []
 
-genesis = read_json_from_file('genesis.json')
+genesis = read_json_from_file("genesis.json")
 
 # For each chapter in the json data:
-for chapter in genesis['chapters']:
+for chapter in genesis["chapters"]:
     # For each verse in the chapter:
-    for verse in chapter['verses']:
+    for verse in chapter["verses"]:
         # Create a document with the verse text
-        genesis_documents.append(verse['text'])
+        genesis_documents.append(verse["text"])
         # Create a metadata object with the chapter and verse number
-        genesis_metadata.append({'chapter': chapter['chapter'], 'verse': verse['verse']})
+        genesis_metadata.append(
+            {"chapter": chapter["chapter"], "verse": verse["verse"]}
+        )
         # Create an id for the document
         genesis_ids.append(f"{chapter['chapter']}:{verse['verse']}")
 
@@ -31,15 +33,24 @@ collection.add(documents=genesis_documents, metadatas=genesis_metadata, ids=gene
 
 print("Size of Collection:", collection.count())
 
-queries = ["God", "God Creates", "Children made in the image of God", "God is love", "God is angry", "How long do people live?"]
-
-pp = pprint.PrettyPrinter(indent=4)
+queries = [
+    "God",
+    "God Creates",
+    "Children made in the image of God",
+    "God is love",
+    "God is angry",
+    "How long do people live?",
+]
 
 for query in queries:
     print(f"Query: {query}")
     results = collection.query(query_texts=[query], n_results=5)
     # Results contains documents, metadatas, and distances
-    for doc, metadata, distance in zip(results['documents'][0], results['metadatas'][0], results['distances'][0]):
-        print(f"- Chapter {metadata['chapter']}, Verse {metadata['verse']} ({distance:.2f}): {doc}")
+    for doc, metadata, distance in zip(
+        results["documents"][0], results["metadatas"][0], results["distances"][0]
+    ):
+        print(
+            f"- Chapter {metadata['chapter']}, Verse {metadata['verse']} ({distance:.2f}): {doc}"
+        )
         print()
     print()
